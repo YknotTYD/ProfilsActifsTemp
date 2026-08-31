0ae53bbb6acee6462d9cd1e6008177c49a47cc20
yknottyd <ethan.bertin-prevot@epitech.eu>
2026-08-31 15:17:57 +0200

    [main] Moved templates - 'pass'.

--- profils/settings.py
+++ profils/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'profils.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["profils/templates"],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
