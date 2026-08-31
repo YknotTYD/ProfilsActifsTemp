12607453a80595e3f00a02cc80f5598a14b99047
yknottyd <ethan.bertin-prevot@epitech.eu>
2026-08-31 16:30:08 +0200

    [main] Added the login/logout logic - 'pass'.

--- profils/mainapp/api.py
+++ profils/mainapp/api.py
@@ -3,13 +3,15 @@ from django.http.request        import HttpRequest
 from django.http.response       import HttpResponse
 from django.shortcuts           import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth        import authenticate, login
+from django.contrib.auth        import authenticate, login as login_
 
 def register(request: HttpRequest) -> HttpResponse:
 
     if "username" in request.POST and "password" in request.POST:
+
         if User.objects.get(username = request.POST["username"]):
-            pass
+            return redirect("/register")
+
         user = User.objects.create_user(
             request.POST["username"],
             None,
@@ -17,7 +19,18 @@ def register(request: HttpRequest) -> HttpResponse:
         )
         user.save()
         auth_user = authenticate(request, username = request.POST["username"], password = request.POST["password"])
-        login(request, auth_user)
+        login_(request, auth_user)
 
+    return redirect("/")
+
+def login(request: HttpRequest) -> HttpResponse:
+
+    if "username" in request.POST and "password" in request.POST:
+        auth_user = authenticate(request, username = request.POST["username"], password = request.POST["password"])
+        if auth_user is None:
+            return redirect("/login")
+        login_(request, auth_user)
+    else:
+        return redirect("/login")
 
     return redirect("/")
--- profils/mainapp/views.py
+++ profils/mainapp/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts     import render
 from django.http.request  import HttpRequest
 from django.http.response import HttpResponse
-from django.shortcuts     import render
-
-# Create your views here.
+from django.shortcuts     import render, redirect
+from django.contrib.auth  import logout as logout_
 
 def main(request: HttpRequest) -> HttpResponse:
-
-    if not request.user.is_authenticated:
-        return register(request)
-
     return render(request, "main.html", {"user": request.user})
 
 def register(request: HttpRequest) -> HttpResponse:
@@ -18,3 +13,18 @@ def register(request: HttpRequest) -> HttpResponse:
         return main(request)
 
     return render(request, "register.html")
+
+def login(request: HttpRequest) -> HttpResponse:
+
+    if request.user.is_authenticated:
+        return main(request)
+
+    return render(request, "login.html")
+
+def logout(request: HttpRequest) -> HttpResponse:
+
+    if not request.user.is_authenticated:
+        return main(request)
+
+    logout_(request)
+    return redirect("/")
