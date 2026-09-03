6ec4d0209629bae2371929c14ca8203021a20a57
ILPinciatore <pierre.maciejewski@epitech.eu>
2026-09-03 10:45:57 +0200

    Fix run server without reload

--- run_server.py
+++ run_server.py
@@ -4,5 +4,5 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'profils.settings')
     from django.core.management import execute_from_command_line
-    sys.argv = ['manage.py', 'runserver', '0.0.0.0:8000']
+    sys.argv = ['manage.py', 'runserver', '0.0.0.0:8000', '--noreload']
     execute_from_command_line(sys.argv)
