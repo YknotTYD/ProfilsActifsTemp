b58300c55767f8037b87bf5ab7558d05d19c34ea
yknottyd <ethan.bertin-prevot@epitech.eu>
2026-09-03 15:08:57 +0200

    [main] Fixed the video file upload - 'pass'.

--- profils/urls.py
+++ profils/urls.py
@@ -39,4 +39,4 @@ urlpatterns = [
     path("",                      include("profils.profiles.urls")),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
