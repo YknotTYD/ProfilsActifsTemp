4ad0d62aebfee5d4b32b02f58bdf2c1a8ce06439
ILPinciatore <pierre.maciejewski@epitech.eu>
2026-09-03 10:40:12 +0200

    Fix exec is now executable

--- run_server.py
+++ run_server.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'monprojet.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'profils.settings')
     from django.core.management import execute_from_command_line
     sys.argv = ['manage.py', 'runserver', '0.0.0.0:8000']
     execute_from_command_line(sys.argv)
